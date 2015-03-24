d43195f670630aab582b71532ccd103d1242885e
root <you@example.com>
2015-03-24 14:30:17 +0200

    ex16

--- j05/ex16.sql
+++ j05/ex16.sql
@@ -1,2 +1,2 @@
 USE db_alakatos;
-SELECT count(*) FROM historique_membre WHERE DATE(date) > DATE('2006-10-30') AND DATE(date) < DATE('2007-07-27');
+SELECT count(*) AS 'films' FROM historique_membre WHERE (DATE(date) > DATE('2006-10-30') AND DATE(date) < DATE('2007-07-27')) OR DATE(date) LIKE "%-12-24";
