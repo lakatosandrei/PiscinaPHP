fae240a206faff349212bdf8ad633736e35c04bc
root <you@example.com>
2015-03-24 09:15:42 +0200

    ex10 si ex11

---
+++ j05/ex10.sql
@@ -0,0 +1,2 @@
+USE db_alakatos;
+SELECT titre AS 'Titre', resum AS 'Resume', annee_prod FROM film INNER JOIN genre ON film.id_genre=genre.id_genre WHERE genre.nom='erotic';
---
+++ j05/ex11.sql
@@ -0,0 +1,2 @@
+USE db_alakatos;
+SELECT UPPER(fiche_personne.nom) AS 'NOM', prenom, abonnement.prix FROM fiche_personne INNER JOIN membre ON fiche_personne.id_perso=membre.id_membre INNER JOIN abonnement ON membre.id_abo=abonnement.id_abo WHERE abonnement.prix >= 42 ORDER BY fiche_personne.nom, fiche_personne.prenom;
