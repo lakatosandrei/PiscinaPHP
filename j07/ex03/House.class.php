ec14e5234cc06744139a5483766373f223dc9dc7
root <you@example.com>
2015-03-26 11:37:26 +0200

    ex03

---
+++ j07/ex03/House.class.php
@@ -0,0 +1,17 @@
+<?php
+	class	House {
+
+		public	function	__construct() {
+			return ;
+		}
+
+		public	function	__destruct() {
+			return ;
+		}
+
+		public	function	introduce() {
+			print "House " . static::getHouseName() . " of " .
+			static::getHouseSeat() . ' : "' . static::getHouseMotto() . '"' . PHP_EOL;
+		}
+	}
+?>
