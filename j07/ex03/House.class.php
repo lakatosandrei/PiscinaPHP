c0a9b1b8bceb2da2593129d35ee367c0ba6f6595
root <you@example.com>
2015-03-26 11:47:34 +0200

    ex03

--- j07/ex03/House.class.php
+++ j07/ex03/House.class.php
@@ -1,17 +1,12 @@
 <?php
-	class	House {
-
-		public	function	__construct() {
-			return ;
-		}
-
-		public	function	__destruct() {
-			return ;
-		}
+	abstract	class	House {
 
 		public	function	introduce() {
 			print "House " . static::getHouseName() . " of " .
 			static::getHouseSeat() . ' : "' . static::getHouseMotto() . '"' . PHP_EOL;
 		}
+		abstract	public	function	getHouseName();
+		abstract	public	function	getHouseMotto();
+		abstract	public	function	getHouseSeat();
 	}
 ?>
